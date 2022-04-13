858a6c704883d0910a8780da0ee1f24794afdcea
braisischanging <braisyt@gmail.com>
2022-04-13 19:34:47 +0200

    moving data to mongodb atlas + upsert logic

--- alphagod/settings.py
+++ alphagod/settings.py
@@ -62,9 +62,12 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'alphagod.pipelines.AlphagodPipeline': 300,
-#}
+
+# Lower number means higher priority
+ITEM_PIPELINES = {
+   'alphagod.pipelines.MongodbPipeline': 300,
+}
+
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
